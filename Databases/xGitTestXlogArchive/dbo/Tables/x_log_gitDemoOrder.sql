e91e265aca70c7eac660fdf671b0c96618371c9a
cigood2750 <cigood2750@gmail.com>
2026-09-09 15:51:52 -0500

    Added two new columns

--- Databases/xGitTestXlogArchive/dbo/Tables/x_log_gitDemoOrder.sql
+++ Databases/xGitTestXlogArchive/dbo/Tables/x_log_gitDemoOrder.sql
@@ -1,9 +1,10 @@
 CREATE TABLE [dbo].[x_log_gitDemoOrder] (
-    [OrderId]   INT            IDENTITY (1, 1) NOT NULL,
-    [WidgetId]  INT            NOT NULL,
-    [Quantity]  INT            NOT NULL,
-    [OrderDate] DATETIME       CONSTRAINT [DF_gitDemoOrder_OrderDate] DEFAULT (getdate()) NOT NULL,
-    [Notes]     NVARCHAR (200) NULL
+    [OrderId]           INT            IDENTITY (1, 1) NOT NULL,
+    [WidgetId]          INT            NOT NULL,
+    [Quantity]          INT            NOT NULL,
+    [OrderDate]         DATETIME       CONSTRAINT [DF_gitDemoOrder_OrderDate] DEFAULT (getdate()) NOT NULL,
+    [Notes]             NVARCHAR (200) NULL,
+    [CognitoNewColumn2] NCHAR (10)     NULL
 );
 
 
